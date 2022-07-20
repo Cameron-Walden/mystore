aa40e7f0405f052789564fddbf33d0e4cb38648d
Cameron <cameronkwalden@gmail.com>
2022-07-20 12:33:23 -0700

    adds, removes, empties cart, adds react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { commerce } from "./commerce/commerce";
+import NavBar from "./components/navbar/NavBar";
+import Products from "./components/products/Products";
+import Cart from "./components/cart/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const [cart, setCart] = useState({});
+
+  const getCart = async () => {
+    const response = await commerce.cart.retrieve();
+    setCart(response);
+  };
+
+  const addToCart = async (productId, quantity) => {
+    const response = await commerce.cart.add(productId, quantity);
+    setCart(response.cart);
+  };
+
+  const updateCartQuantity = async (id, quantity) => {
+    const response = await commerce.cart.update(id, { quantity });
+    setCart(response.cart);
+  };
+
+  const removeFromCart = async (id) => {
+    const response = await commerce.cart.remove(id);
+    setCart(response.cart);
+  };
+
+  const emptyCart = async () => {
+    const response = await commerce.cart.empty();
+    setCart(response.cart);
+  };
+
+  useEffect(() => {
+    getCart();
+  }, []);
+
+  // console.log(cart, '<==cart')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar cart={cart} />
+        <Routes>
+          <Route
+            path="/"
+            element={<Products commerce={commerce} onAddToCart={addToCart} />}
+          ></Route>
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                cart={cart}
+                updateCartQuantity={updateCartQuantity}
+                removeFromCart={removeFromCart}
+                emptyCart={emptyCart}
+              />
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
