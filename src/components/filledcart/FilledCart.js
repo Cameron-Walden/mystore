9986b4bd43fd0f17e7a8fee945a3cff4155ed64e
Cameron <cameronkwalden@gmail.com>
2022-07-24 14:20:18 -0700

    swaps out useStyles for css

--- src/components/filledcart/FilledCart.js
+++ src/components/filledcart/FilledCart.js
@@ -1,54 +1,54 @@
-import { Grid, Typography, Button } from "@mui/material";
+import { Grid, Typography, Button, CircularProgress } from "@mui/material";
 import CartItem from "../cartitem/CartItem";
-import useStyles from "./filledCartStyles";
+import './FilledCart.css';
 
-export default function FilledCart({
-  cart,
-  updateCartQuantity,
-  removeFromCart,
-  emptyCart,
-}) {
-  const classes = useStyles();
+export default function FilledCart({ cart, updateCartQuantity, removeFromCart, emptyCart }) {
 
   return (
     <>
-      <Grid container spacing={3}>
-        {cart.line_items.map((cartItem) => (
-          <Grid item={true} sm={8} md={6} lg={4} key={cartItem.id}>
-            <CartItem
-              cartItem={cartItem}
-              updateCartQuantity={updateCartQuantity}
-              removeFromCart={removeFromCart}
-            />
+      {!cart.line_items ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Grid container spacing={3}>
+            {cart.line_items.map((cartItem) => (
+              <Grid item={true} sm={8} md={6} lg={4} key={cartItem.id}>
+                <CartItem
+                  cartItem={cartItem}
+                  updateCartQuantity={updateCartQuantity}
+                  removeFromCart={removeFromCart}
+                />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      <div className={classes.cardDetails}>
-        <Typography variant="h4">
-          Subtotal: {cart.subtotal.formatted_with_symbol}
-        </Typography>
-        <div>
-          <Button
-            className={classes.emptyCartButton}
-            size="large"
-            type="button"
-            variant="contained"
-            color="secondary"
-            onClick={emptyCart}
-          >
-            Empty Cart
-          </Button>
-          <Button
-            className={classes.checkoutButton}
-            size="large"
-            type="button"
-            variant="contained"
-            color="primary"
-          >
-            Checkout
-          </Button>
-        </div>
-      </div>
+          <div className="cardDetails">
+            <Typography variant="h4">
+              Subtotal: {cart.subtotal.formatted_with_symbol}
+            </Typography>
+            <div>
+              <Button
+                className="emptyCartButton"
+                size="large"
+                type="button"
+                variant="contained"
+                color="secondary"
+                onClick={emptyCart}
+              >
+                Empty Cart
+              </Button>
+              <Button
+                className="checkoutButton"
+                size="large"
+                type="button"
+                variant="contained"
+                color="primary"
+              >
+                Checkout
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
